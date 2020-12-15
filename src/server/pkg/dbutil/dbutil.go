93d8bb846c611f4dc27ae34a7742abab6c6275ea
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-12-15 14:06:04 -0500

    fix postgres ephemeral db names

--- src/server/pkg/dbutil/dbutil.go
+++ src/server/pkg/dbutil/dbutil.go
@@ -2,8 +2,8 @@ package dbutil
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/pachyderm/pachyderm/src/client/pkg/require"
@@ -27,20 +27,34 @@ const (
 func NewTestDB(t testing.TB) *sqlx.DB {
 	dsn := fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", DefaultPostgresHost, DefaultPostgresPort, TestPostgresUser)
 	db := sqlx.MustOpen("postgres", dsn)
-	dbName := fmt.Sprintf("test_%d", time.Now().UnixNano())
-	db.MustExec("CREATE DATABASE " + dbName)
-	t.Log("database", dbName, "successfully created")
+	dbName := ephemeralDBName()
+	t.Log("creating database:", dbName)
+	db.MustExec(`CREATE DATABASE ` + dbName)
 	t.Cleanup(func() {
 		if !devDontDropDatabase {
 			db.MustExec("DROP DATABASE " + dbName)
 		}
 		require.NoError(t, db.Close())
 	})
-	db2 := sqlx.MustOpen("postgres", dsn+" dbname="+dbName)
+	db2 := sqlx.MustConnect("postgres", dsn+" dbname="+dbName)
+	t.Log("database", dbName, "successfully created")
 	t.Cleanup(func() {
 		require.NoError(t, db2.Close())
 	})
 	return db2
+}
+
+func ephemeralDBName() string {
+	// TODO: it looks like postgres is truncating identifiers to 32 bytes,
+	// it should be 64 but we might be passing the name as non-ascii, i'm not really sure.
+	// for now just use a random int, but it would be nice to go back to names with a timestamp.
+	return fmt.Sprintf("test_%08x", rand.Uint64())
+	//now := time.Now()
+	// test_<date>T<time>_<random int>
+	// return fmt.Sprintf("test_%04d%02d%02dT%02d%02d%02d_%04x",
+	// 	now.Year(), now.Month(), now.Day(),
+	// 	now.Hour(), now.Minute(), now.Second(),
+	// 	rand.Uint32())
 }
 
 // DBParams are parameters passed to the db constructor.
